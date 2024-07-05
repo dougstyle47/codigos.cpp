fb38342fe52c25e2fa18b90411f4c84a04da9dad
dougstyle47 <104530560+dougstyle47@users.noreply.github.com>
2024-07-05 00:18:34 -0300

    adicionando sistema de pontuação no jogo

--- adivinhacao.cpp
+++ adivinhacao.cpp
@@ -1,4 +1,4 @@
-#include <iostream> //Include=Entrada Output=Saida STREAM=fluxo
+#include <iostream> //i=include=Entrada o=Output=Saida STREAM=fluxo
 using namespace std;
 
 
@@ -7,19 +7,44 @@ int main(){
     cout <<"*Bem vindos ao jogo da adivinhacao!*" << endl;
     cout <<"************************************" << endl;
 
-    int secret_number = 47;
-    cout <<" O numero secreto e " << secret_number << " nao conte para ninguem" << endl;
+    const int SECRET_NUMBER = 47;
+    bool notHit = true;
+    int attempts = 0;
 
-    int shot;
-    cout << "Qual o valor do seu chute?" <<endl;
-    cin >> shot;
-    cout << "O valor do seu chute e: " << shot << endl;
+    double points = 1000.0;
 
-    if(shot == secret_number){
-        cout << "Parabens voce acertou o numero secreto" << endl;
-    } else if(shot > secret_number){
-        cout << "O numero que voce chutou e maior que o numero secreto" << endl;  
-    } else{
-        cout << "O numero que voce chutou e menor que o numero secreto" << endl;
-    }
+    while(notHit){
+        attempts++;
+        int shot;
+
+        cout << "Tentativa " << attempts <<endl;
+        
+        cout << "Qual o valor do seu chute?" <<endl;
+        cin >> shot;
+
+        //abs = para retornar o valor absoluto ou seja somente valores positivos
+        double lostPoints = abs(shot-SECRET_NUMBER)/2.0;
+        points = points - lostPoints;
+
+        cout << "O valor do seu chute e: " << shot << endl;
+
+        bool hit = shot == SECRET_NUMBER;
+        bool biggerHit = shot > SECRET_NUMBER;
+
+        if(hit){
+            cout << "Parabens voce acertou o numero secreto" << endl;
+            notHit = false;
+        } else if(biggerHit){
+            cout << "O numero que voce chutou e maior que o numero secreto" << endl;  
+        } else{
+            cout << "O numero que voce chutou e menor que o numero secreto" << endl;
+        }
+
+    } 
+    cout << "GAME'S OVER"<< endl;
+    cout << "Voce acertou o numero secreto em: " << attempts << " tentativas"<< endl;
+    cout.precision(2); //para setar quantos números após a virgula nois queremos
+    cout << fixed; //para não sair na notação cientifica
+    cout << "Sua pontuacao foi de " << points << " pontos" << endl;
+
 }
