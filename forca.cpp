cc03c1aa13ab7f23abd62cd819b2c81a75106c9c
dougstyle47 <104530560+dougstyle47@users.noreply.github.com>
2024-07-09 23:19:22 -0300

    Adicionando novas funcionalidades no jogo da forca

--- forca.cpp
+++ forca.cpp
@@ -1,35 +1,64 @@
 #include <iostream> //Ela inclui as var tipo string, mas é uma boa prática colocar
 #include <string>
+#include <map>
+#include <vector>
 using namespace std;
 
 //Está fora do escopo pois é uma variável global
 const string SECRET_WORD = "DOUGLAS"; 
+map<char, bool> shoted; //nao entendi
+vector<char> missedAttempt;
 
-//Função para verificar se a letra existe ou não
+//Função para verificar se a letra existe ou não na palavra
 bool existWord(char shot){
-    for(int i = 0; i < SECRET_WORD.size(); i++ ){
-        if(shot == SECRET_WORD[i]){
+   for(char word : SECRET_WORD)
+        if(shot == word){
             return true;
-        }
-   }
+        }   
    return false;
 }
 
 int main(){
+
+    cout << endl;
+    cout << "*******************************" <<endl;
+    cout << "********JOGO DA FORCA**********" <<endl;
+    cout << "*******************************" <<endl;
+    cout << endl;
+
     cout << SECRET_WORD << endl;
 
     bool notHit = true;
     bool notHanger = true;
 
     while(notHit && notHanger){
+        cout <<"Chutes errados: " ;
+        for(char word : missedAttempt){
+            cout << word << " " ;
+        }
+        cout << endl;
+
+        //não entendi
+        for(char word : SECRET_WORD){
+            if(shoted[word]){
+                cout << word << " ";
+            }else{
+                cout << "_ ";
+            }
+        }
+        cout << endl;
+
+        cout << "Seu chute: ";
         char shot;
         cin >> shot;
-
+        shoted [shot] = true; //não entendi
 
         if(existWord(shot)){
             cout << "Voce acertou, seu chute esta na palavra" << endl;
         }else{
             cout << "Voce errou, seu chute nao esta na palavra" <<endl;
+            missedAttempt.push_back(shot);
         }
+        cout << endl;
     }
 }
