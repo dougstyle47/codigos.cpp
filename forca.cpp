166ee7854948d86511d5f4c9c77279f1ca625efa
dougstyle47 <104530560+dougstyle47@users.noreply.github.com>
2024-07-11 00:07:20 -0300

    Adicionando funções para diminuir o main

--- forca.cpp
+++ forca.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <map>
 #include <vector>
+#include <fstream> //Para colocar o ifstream 
 using namespace std;
 
 //Está fora do escopo pois é uma variável global
@@ -18,47 +19,97 @@ bool existWord(char shot){
    return false;
 }
 
-int main(){
+bool notHit(){
+    for(char word : SECRET_WORD){
+        if(!shoted[word]){
+            return true;
+        }
+    }
+    return false;
+}
 
-    cout << endl;
+bool notHanger(){
+    return missedAttempt.size() < 5;
+}
+
+void printHeader(){
+     cout << endl;
     cout << "*******************************" <<endl;
     cout << "********JOGO DA FORCA**********" <<endl;
     cout << "*******************************" <<endl;
     cout << endl;
+}
 
-    cout << SECRET_WORD << endl;
-
-    bool notHit = true;
-    bool notHanger = true;
-
-    while(notHit && notHanger){
-        cout <<"Chutes errados: " ;
-        for(char word : missedAttempt){
-            cout << word << " " ;
-        }
-        cout << endl;
-
-        //não entendi
-        for(char word : SECRET_WORD){
-            if(shoted[word]){
-                cout << word << " ";
-            }else{
-                cout << "_ ";
-            }
-        }
-        cout << endl;
-
-        cout << "Seu chute: ";
-        char shot;
-        cin >> shot;
-        shoted [shot] = true; //não entendi
-
-        if(existWord(shot)){
-            cout << "Voce acertou, seu chute esta na palavra" << endl;
-        }else{
-            cout << "Voce errou, seu chute nao esta na palavra" <<endl;
-            missedAttempt.push_back(shot);
-        }
-        cout << endl;
+void printMistakes(){
+    cout <<"Chutes errados: " ;
+    for(char word : missedAttempt){
+        cout << word << " " ;
     }
+    cout << endl;
+}
+
+//não entendi essa função
+void printWord(){
+    for(char word : SECRET_WORD){
+        if(shoted[word]){
+          cout << word << " ";
+        }else{
+            cout << "_ ";
+        }
+    }
+        cout << endl;
+}
+
+void shot(){
+    cout << "Seu chute: ";
+    char shot;
+    cin >> shot;
+    shoted [shot] = true; //não entendi
+
+    if(existWord(shot)){
+        cout << "Voce acertou, seu chute esta na palavra" << endl;
+    }else{
+         cout << "Voce errou, seu chute nao esta na palavra" <<endl;
+         missedAttempt.push_back(shot);  
+    }     
+    cout << endl;
+}
+
+vector<string> readFile(){
+    //input, file, stream
+    ifstream file;
+    file.open("words.txt");
+
+    int numberWords;
+    file >> numberWords;
+
+    cout << "O arquivo possui " << numberWords << " palavras." <<endl;
+
+    vector<string> fileWords;
+    
+
+    for(int i = 0; i < numberWords; i++){
+        string readWord;
+        file >> readWord;
+        cout << "Na linha " << i << " :" << readWord << endl; 
+    }
+}
+
+int main(){
+    printHeader();
+
+    readFile();
+
+    while(notHit() && notHanger()){
+        printMistakes();
+        printWord();
+        shot();
+    }
+    cout << "Fim de jogo" << endl;
+    cout << "A palavra secreta era: " << SECRET_WORD << endl;
+    if(notHit()){
+        cout << "Voce perdeu! Tente novamente!" << endl;
+    } else {
+        cout << "Parabens! Você acertou a palavra secreta "<< endl;
+    }  
 }
