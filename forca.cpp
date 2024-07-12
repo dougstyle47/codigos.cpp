242ec716406c357262fc888d5254874031d9c562
dougstyle47 <104530560+dougstyle47@users.noreply.github.com>
2024-07-12 00:46:40 -0300

    Terminando o jogo da forca

--- forca.cpp
+++ forca.cpp
@@ -3,16 +3,18 @@
 #include <map>
 #include <vector>
 #include <fstream> //Para colocar o ifstream 
+#include <ctime>
+#include <cstdlib>
 using namespace std;
 
 //Está fora do escopo pois é uma variável global
-const string SECRET_WORD = "DOUGLAS"; 
+string secretWord = "DOUGLAS"; 
 map<char, bool> shoted; //nao entendi
 vector<char> missedAttempt;
 
 //Função para verificar se a letra existe ou não na palavra
 bool existWord(char shot){
-   for(char word : SECRET_WORD)
+   for(char word : secretWord)
         if(shot == word){
             return true;
         }   
@@ -20,7 +22,7 @@ bool existWord(char shot){
 }
 
 bool notHit(){
-    for(char word : SECRET_WORD){
+    for(char word : secretWord){
         if(!shoted[word]){
             return true;
         }
@@ -50,7 +52,7 @@ void printMistakes(){
 
 //não entendi essa função
 void printWord(){
-    for(char word : SECRET_WORD){
+    for(char word : secretWord){
         if(shoted[word]){
           cout << word << " ";
         }else{
@@ -80,25 +82,75 @@ vector<string> readFile(){
     ifstream file;
     file.open("words.txt");
 
-    int numberWords;
-    file >> numberWords;
+    //if para se caso o arquivo for mudado de lugar, exibir uma mensagem de erro
+    if(file.is_open()){ 
+        int numberWords;
+        file >> numberWords;
 
-    cout << "O arquivo possui " << numberWords << " palavras." <<endl;
+        //cout << "O arquivo possui " << numberWords << " words." <<endl;
 
-    vector<string> fileWords;
-    
+        vector<string> fileWords;
 
-    for(int i = 0; i < numberWords; i++){
-        string readWord;
-        file >> readWord;
-        cout << "Na linha " << i << " :" << readWord << endl; 
+        for(int i = 0; i < numberWords; i++){
+            string readWord;
+            file >> readWord;
+            //cout <<" Na linha " << i << " :" << readWord <<endl;
+            fileWords.push_back(readWord);
+        }
+        file.close(); //boa pratica é fechar o arquivo após o uso dele
+        return fileWords;
+    } else {
+        cout << "Não foi possível acessar o banco de palavras." << endl;
+        exit(0);
     }
 }
+
+//draw = sortear
+void drawWord(){
+    vector<string> words = readFile();
+
+    srand(time(NULL));
+    int drawIndex = rand() % words.size();
+
+    secretWord = words[drawIndex];
+}
+
+
+void saveFile(vector<string> newList){
+    ofstream file;
+    file.open("words.txt");
+
+    if(file.is_open()){
+        file << newList.size() << endl;
+
+        for(string word : newList){
+            file << word << endl;
+            //cout << "Palavra adicionada com sucesso." << endl;
+        }
+        file.close();
+    }else{
+        cout << "Não foi possível acessar o banco de palavras." << endl;
+        exit(0);
+    }
+}
+
+void addWord(){
+    cout << "Digite a nova palavra, usando letras maiusculas." << endl;
+    string newWord;
+    cin >> newWord;
+
+    vector<string> listWords = readFile();
+    listWords.push_back(newWord);
+
+    saveFile(listWords);
+}
+
 
 int main(){
     printHeader();
 
     readFile();
+    drawWord();
 
     while(notHit() && notHanger()){
         printMistakes();
@@ -106,10 +158,18 @@ int main(){
         shot();
     }
     cout << "Fim de jogo" << endl;
-    cout << "A palavra secreta era: " << SECRET_WORD << endl;
+    cout << "A palavra secreta era: " << secretWord << endl;
     if(notHit()){
         cout << "Voce perdeu! Tente novamente!" << endl;
     } else {
-        cout << "Parabens! Você acertou a palavra secreta "<< endl;
+        cout << "Parabens! Voce acertou a palavra secreta "<< endl;
+        cout << "Voce deseja adicionar uma nova palavra ao banco(S/N)" << endl;
+        char answer;
+        cin >> answer;
+        if(answer == 'S'){
+            addWord();
+        } else {
+
+        }
     }  
 }
